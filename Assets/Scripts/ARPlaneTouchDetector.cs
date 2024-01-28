59ee955101263d4246d53fd628c6ce0942f07012
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2024-01-28 05:13:17 -0600

    Main scene update

--- Assets/Scripts/ARPlaneTouchDetector.cs
+++ Assets/Scripts/ARPlaneTouchDetector.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using TMPro;
@@ -7,91 +6,40 @@ using System.Collections.Generic;
 
 public class ARPlaneTouchDetector : MonoBehaviour
 {
-    //public TMP_Text touchResultText; // Asigna el componente TextMeshProUGUI del Canvas a trav�s del Inspector
-    public ARRaycastManager aRRaycastManager;
-    public ARPlaneManager aRPlaneManager;
-    public List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    public GameObject prefab;
-    public TextMeshProUGUI text;
-    private Camera arCamera;
+    [SerializeField] private float raycastDisntance;
+    
+    private Vector2 touchPosition = default;
+    Camera m_MainCamera;
 
-
-    private bool prefabInstantiated = false;
-
-    void Start()
-    {
-        aRRaycastManager = GetComponent<ARRaycastManager>();
-        aRPlaneManager = GetComponent<ARPlaneManager>();
-        hits = new List<ARRaycastHit>();
+    private void Start() {
+        m_MainCamera = Camera.main;
     }
-
-    void Update()
+ 
+    void Update() 
     {
-        /*if (Input.touchCount > 0 && !prefabInstantiated)
+        if(Input.touchCount > 0) 
         {
-            Debug.Log("here");
-            
             Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = touch.position;
+            touchPosition = touch.position;
+ 
+            //Checking to see if the position of the touch is over a UI object in case of UI overlay on screen.
 
-            if (aRRaycastManager != null /*&& touchResultText != null *//*)
+            Ray ray = m_MainCamera.ScreenPointToRay(touchPosition);
+            RaycastHit hitObject;
+
+            if (Physics.Raycast(ray, out hitObject)) 
             {
-                if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+                if (hitObject.distance < raycastDisntance)
                 {
-                    // Se ha detectado un plano
-                    //touchResultText.text = "Tocaste un plano";
-                    
-                    foreach (ARRaycastHit hit in hits)
-                    {
-                        Pose pose = hit.pose;
-                        GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
-                        Debug.Log(hit.trackable.gameObject.tag);
-                    }
+                    Debug.Log(hitObject.transform.gameObject.name);
+                    Debug.Log(hitObject.transform.gameObject.tag);
 
-                    // Marcamos que ya se ha instanciado el prefab
-                    prefabInstantiated = true;
-
-                    // Desactivar AR Plane Manager y trackables
-                    if (aRPlaneManager != null)
+                    if (hitObject.transform.gameObject.GetComponent<BreakObject>())
                     {
-                        aRPlaneManager.enabled = false; // Desactiva AR Plane Manager
-                        foreach (var plane in aRPlaneManager.trackables)
-                        {
-                            plane.gameObject.SetActive(false); // Desactiva cada plano individual
-                        }
+                        hitObject.transform.gameObject.GetComponent<BreakObject>().TakeDamage(1);
                     }
                 }
-                else
-                {
-                    // No se ha detectado un plano
-                    ///touchResultText.text = "No tocaste un plano";
-                }
-            }
-        }*/
-        
-        if (Input.touchCount == 0)
-            return;
-
-        Touch touch = Input.GetTouch(0);
-
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        if (aRRaycastManager.Raycast(touch.position, hits))
-        {
-            // Raycast hits an AR plane or tracked image
-            Pose hitPose = hits[0].pose;
-
-            // Perform a traditional raycast from the camera to the hit position
-            RaycastHit hitInfo;
-            if (Physics.Raycast(arCamera.ScreenPointToRay(touch.position), out hitInfo))
-            {
-                // Check if the raycast hit a GameObject
-                GameObject hitObject = hitInfo.collider.gameObject;
-
-                // Retrieve the tag of the hit GameObject
-                string objectTag = hitObject.tag;
-                Debug.Log("Hit object tag: " + objectTag);
             }
         }
-        
     }
 }
