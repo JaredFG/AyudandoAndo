fd4a16616c232f2c11d62fd769335d747c570f2b
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2024-01-28 16:13:50 -0600

    Add functional UI

--- Assets/Scripts/Repair Tasks/RepairTaskManager.cs
+++ Assets/Scripts/Repair Tasks/RepairTaskManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RepairTaskManager : MonoBehaviour
 {
@@ -9,11 +12,16 @@ public class RepairTaskManager : MonoBehaviour
     [Header("Task Managers")]
 
     [SerializeField] private ToiletTaskManager toiletTaskManager;
+    [SerializeField] private GameSceneManager gameSceneManager;
 
     [Header("Session variables")] 
     [SerializeField] private int taskHp;
     [SerializeField] private int tasksDone;
-    
+
+    [Header("UI And GameStates")] 
+    [SerializeField] private Image integrityBar;
+    [SerializeField] private TextMeshProUGUI textLabel;
+
     private void Awake() 
     { 
         // If there is an instance, and it's not me, delete myself.
@@ -25,6 +33,12 @@ public class RepairTaskManager : MonoBehaviour
         { 
             Instance = this; 
         } 
+    }
+
+    private void Update()
+    {
+        integrityBar.fillAmount = (taskHp / 3);
+        textLabel.text = taskHp.ToString();
     }
 
     public void Toilet_PerformTask(int taskId)
@@ -46,13 +60,17 @@ public class RepairTaskManager : MonoBehaviour
     
     private void CheckGameState()
     {
+        Debug.Log($"TaskHP = %{taskHp}");
+        
         if (taskHp <= 0)
         {
+            gameSceneManager.ShowLoseCanvas();
             Debug.Log("GAME OVER");
         }
 
         if (tasksDone >= 2)
         {
+            gameSceneManager.ShowWinCanvas();
             Debug.Log("YOU WIN");
         }
     }
